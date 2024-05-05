c01f5b1f3da800e6b894712f626ede1821870055
Khánh Phùng <91212840+pmk2k1@users.noreply.github.com>
2024-05-06 02:52:02 +0700

    Improvement - Convert database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_05_05_184539) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blacklisted_tokens", force: :cascade do |t|
-    t.integer "users_id", null: false
+    t.bigint "users_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["users_id"], name: "index_blacklisted_tokens_on_users_id"
